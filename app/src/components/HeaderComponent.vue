cfbcdd511fe6da15db1c8eca1bd95d1081829849
ChihChunHan <chihchun.han@gmail.com>
2020-09-20 16:10:53 +0800

    [feat] add header & logo components

---
+++ app/src/components/HeaderComponent.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="list">
+    <div class="cate" v-for="item of listData" :key="item.title">
+      {{item.title}}
+      <span>{{item.sub}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      listData: [
+        { title: "活動視覺設計", sub: "Event Visual Design" },
+        { title: "空間繪圖設計", sub: "3D Model & Render" },
+        { title: "網頁前端設計", sub: "Web Front-end & Design" }
+        ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.list{
+    color: 333;
+    font-size: 1rem;
+    font-weight: 300;
+    letter-spacing: 2px;
+    display: flex;
+    justify-content: space-around;
+}
+
+.list .cate {
+    margin-right: 3rem;
+    margin-bottom: 1rem;
+    line-height: 1.2rem;
+    padding-left: 0.5rem;
+    border-left: 3px solid #000;
+}
+
+.list .cate span {
+    margin-left: 0.5rem;
+    font-size: 1rem;
+}
+
+@media (max-width: 992px) {
+    .list{
+        margin-top: 2rem;
+        font-size: 14px;
+        flex-direction: column;
+        text-align: end;
+    }
+
+    .list .cate {
+        margin-right: 0;
+        border-left: 0;
+    }
+
+    .list .cate span {
+        font-size: 8px;
+        display: block;
+    }
+}
+
+</style>
