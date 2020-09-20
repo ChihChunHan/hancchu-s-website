d166ea8bb2b5e0f30fbc3b423a461ac79f2151e2
ChihChunHan <chihchun.han@gmail.com>
2020-09-20 16:10:30 +0800

    [feat] init vue-cli

---
+++ app/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <HeaderComponent></HeaderComponent>
+    <LogoSvg class="col-lg-12 col-3 title"></LogoSvg>
+  </div>
+</template>
+
+<script>
+import LogoSvg from "./components/LogoSvg";
+import HeaderComponent from "./components/HeaderComponent";
+
+export default {
+  name: "App",
+  components: {
+    LogoSvg,
+    HeaderComponent,
+  },
+};
+</script>
+
+<style>
+</style>
