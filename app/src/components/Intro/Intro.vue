04eae114586320fae32f3d1258593f824b407b76
ChihChunHan <chihchun.han@gmail.com>
2020-10-14 21:39:36 +0800

    add Intro component

---
+++ app/src/components/Intro/Intro.vue
@@ -0,0 +1,207 @@
+<template>
+  <div id="intro">
+    <div class="blink container" style="height: 15vh"></div>
+    <div class="sd-intro index">Intro</div>
+    <div class="intro" style="position: relative">
+      <div class="container">
+        <div class="title">
+          <small>01</small>
+          <div class="txt">Event Visual Design</div>
+          <div class="sub-title" data-wow-delay="0.2s">活動視覺設計</div>
+        </div>
+        <div class="row p-3 mb-5">
+          <div class="col-lg-4 col-12 p-0">
+            <img src="images/intro/LPS_ARCADIA-ELROW_2018_0661.JPG" />
+            <div class="card-txt">
+              EDM-party
+              <small>電音派對活動</small>
+            </div>
+          </div>
+          <div class="col-lg-4 col-12 p-0" data-wow-delay=".2s">
+            <img src="images/intro/IMG_0418.jpg" />
+            <div class="card-txt">
+              Physical activity
+              <small>實體活動</small>
+            </div>
+          </div>
+          <div class="col-lg-4 col-12 p-0" data-wow-delay=".4s">
+            <img src="images/intro/IMG_0147.jpg" />
+            <div class="card-txt">
+              Concert
+              <small>演唱會</small>
+            </div>
+          </div>
+          <div class="txt" data-wow-delay=".6s">
+            2017-19在娛樂產業設計執行了大大小小的活動，在與時間賽跑的過程中堅持產出有美感的設計。
+          </div>
+        </div>
+        <div class="row p-3">
+          <div class="col-lg-9 col-12 p-0 ml-auto">
+            <div class="title ml-auto">
+              <small>02</small>
+              <div>3D Modle & Render</div>
+              <div class="sub-title" data-wow-delay="0.2s">3D空間繪圖設計</div>
+            </div>
+            <div>
+              <img src="images/intro/View 0_5.jpg" />
+              <small>-PRADA慈善晚會</small>
+              <div class="txt">
+                從學生時代開始研究與自學不同的建模與渲染軟體，範圍囊括產品、動畫、空間設計。讓提案時就可以與客戶有最完整的討論空間，尤其在大型活動的執行與跨部門合作時讓我可以知道更全面性的需求。
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row p-3">
+          <div class="col-lg-9 p-0 mr-auto">
+            <div class="title">
+              <small>03</small>
+              <div>Web Front-end & Design</div>
+              <div class="sub-title" data-wow-delay="0.2s">網頁前端設計</div>
+            </div>
+            <div>
+              <img src="images/intro/web-04-aclock.png" />
+              <div class="txt">
+                對前端有著強烈學習興趣，喜歡研究視覺互動與視覺特效表現方式。藉由網頁前端試著表現出平面設計無法達成的互動關係。
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="project row py-5">
+          <div class="col-auto w-100 d-flex flex-column justify-content-center">
+            <div class="title mx-auto" style="display: inline-block">
+              <small>04</small>
+              <div>Participate in the project</div>
+              <div class="sub-title" data-wow-delay="0.2s">參與大型專案</div>
+            </div>
+            <div class="mx-auto wrap">
+              <div class="row">
+                <div class="col-lg-6 col-12">
+                  <div class="txt">萬人電音趴</div>
+                </div>
+                <div class="col-lg-6 col-12">
+                  <div class="txt">
+                    <ul>
+                      <li>
+                        <div class="item">elrow ＠ Cosmos Live！Graduation</div>
+                        <span class="badge badge-secondary">2019</span>
+                      </li>
+                      <li>
+                        <div class="item">Arcadia Taiwan－FINAL LANDING</div>
+                        <span class="badge badge-secondary">2018</span>
+                      </li>
+                      <li>
+                        <div class="item">Arcadia Taiwan－Invasion</div>
+                        <span class="badge badge-secondary">2017</span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+
+                <div class="col-lg-6 col-12">
+                  <div class="txt">妝髮造型大秀</div>
+                </div>
+                <div class="col-lg-6 col-12">
+                  <div class="txt">
+                    <ul>
+                      <li>
+                        <div class="item">AlternativeHairShow</div>
+                        <span class="badge badge-secondary">2019</span>
+                      </li>
+                      <li>
+                        <div class="item">KEVIN.MURPHY大秀</div>
+                        <span class="badge badge-secondary">2018</span>
+                      </li>
+                      <li>
+                        <div class="item">施華蔻120週年髮型秀</div>
+                        <span class="badge badge-secondary">2018</span>
+                      </li>
+                      <li>
+                        <div class="item">KEVIN.MURPHY大秀</div>
+                        <span class="badge badge-secondary">2017</span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+
+                <div class="col-lg-6 col-12">
+                  <div class="txt">索尼創意科學大賞</div>
+                </div>
+                <div class="col-lg-6 col-12">
+                  <div class="txt">
+                    <ul>
+                      <li>
+                        <div class="item">第七屆索尼創意科學大賞</div>
+                        <span class="badge badge-secondary">2019</span>
+                      </li>
+                      <li>
+                        <div class="item">第六屆索尼創意科學大賞</div>
+                        <span class="badge badge-secondary">2018</span>
+                      </li>
+                      <li>
+                        <div class="item">第五屆索尼創意科學大賞</div>
+                        <span class="badge badge-secondary">2017</span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+
+                <div class="col-lg-6 col-12">
+                  <div class="txt">商業實體活動</div>
+                </div>
+                <div class="col-lg-6 col-12">
+                  <div class="txt">
+                    <ul>
+                      <li>
+                        <div class="item">Fab15B台積電 尾牙</div>
+                        <span class="badge badge-secondary">2019</span>
+                      </li>
+                      <li>
+                        <div class="item">乾杯集團尾牙</div>
+                        <span class="badge badge-secondary">2018</span>
+                      </li>
+                      <li>
+                        <div class="item">
+                          讓EYE看得見萬聖瞳樂會–臺灣防盲基金會
+                        </div>
+                        <span class="badge badge-secondary">2018</span>
+                      </li>
+                      <li>
+                        <div class="item">湯森路透理柏台灣基金獎</div>
+                        <span class="badge badge-secondary">2018</span>
+                      </li>
+                      <li>
+                        <div class="item">LMS英雄聯盟夏季季後賽</div>
+                        <span class="badge badge-secondary">2017</span>
+                      </li>
+                      <li>
+                        <div class="item">ettoday好朋友野餐日</div>
+                        <span class="badge badge-secondary">2017</span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="preview">
+          <div class="box">
+            <div class="txt">Project Image</div>
+          </div>
+          <div class="arrow"></div>
+        </div>
+        <div class="txtEfct"></div>
+        <div class="cateEfct">
+          <div class="white"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
