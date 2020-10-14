5a0d46de4b7aa0045554c4a65c725b227c8b9d7c
ChihChunHan <chihchun.han@gmail.com>
2020-10-14 21:35:36 +0800

    add Footer component

--- app/src/App.vue
+++ app/src/App.vue
@@ -435,36 +435,7 @@
             </div>
           </div>
         </div>
-        <div
-          id="contacts"
-          class="contacts"
-          style="position: relative;background-color: #fff;z-index: 2;"
-        >
-          <div class="index">Contacts</div>
-          <div class="container" style="height: 100vh;">
-            <div class="title">designer</div>
-            <div class="row designer">
-              <div class="col-lg-9 col-auto p-0">
-                <img class="mx-auto" src="images/myphoto.jpg" />
-              </div>
-              <div class="col-lg-3 col-auto name">
-                <div class="chi">韓志群</div>
-                <div class="eng">chih- chun, han</div>
-              </div>
-            </div>
-            <div class="email">chihchun.han@gmail.com</div>
-            <div class="phone">+886 918 518 051</div>
-            <div class="URL">
-              <a href="https://github.com/ChihChunHan" target="_blank">
-                <img src="images/github-brands.svg" alt />
-              </a>
-              <a href="https://www.behance.net/frankhan09c670" target="_blank">
-                <img src="images/behance-square-brands.svg" alt />
-              </a>
-            </div>
-            <div class="right">© hancchun all rights reserved.</div>
-          </div>
-        </div>
+        <Footer></Footer>
       </div>
     </div>
   </div>
@@ -474,11 +445,14 @@
 // import LogoSvg from "./components/LogoSvg";
 // import HeaderComponent from "./components/HeaderComponent";
 
+import Footer from "./components/Footer/Footer"
+
 export default {
   name: "App",
   components: {
     // LogoSvg,
     // HeaderComponent,
+    Footer,
   },
 };
 </script>
---
+++ app/src/components/Footer/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    id="contacts"
+    class="contacts"
+    style="position: relative;background-color: #fff;z-index: 2;"
+  >
+    <div class="index">Contacts</div>
+    <div class="container" style="height: 100vh;">
+      <div class="title">designer</div>
+      <div class="row designer">
+        <div class="col-lg-9 col-auto p-0">
+          <img class="mx-auto" src="images/myphoto.jpg" />
+        </div>
+        <div class="col-lg-3 col-auto name">
+          <div class="chi">韓志群</div>
+          <div class="eng">chih- chun, han</div>
+        </div>
+      </div>
+      <div class="email">chihchun.han@gmail.com</div>
+      <div class="phone">+886 918 518 051</div>
+      <div class="URL">
+        <a href="https://github.com/ChihChunHan" target="_blank">
+          <img src="images/github-brands.svg" alt />
+        </a>
+        <a href="https://www.behance.net/frankhan09c670" target="_blank">
+          <img src="images/behance-square-brands.svg" alt />
+        </a>
+      </div>
+      <div class="right">© hancchun all rights reserved.</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
